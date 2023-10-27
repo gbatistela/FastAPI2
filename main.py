9357e29aa3c5df38bfaea01ee935c40219744018
gbatistela <giulianobatistela@gmail.com>
2023-10-27 10:20:54 -0300

    tercero

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 # Obtenemos el dataframe
 import pandas as pd
-df_games = pd.read_csv("df_games.csv")
+df_games = pd.read_csv("C:\\Users\\Giuliano\\Desktop\\PI MLOps - STEAM\\df_games.csv")
 
 @app.get("/Developer")
 def Developer(developer:str):
